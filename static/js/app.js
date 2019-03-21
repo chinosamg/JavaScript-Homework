fac567a493a380a315f8051831f0028eee06ad25
Samuel Guzman <[chinosamg@gmail.com]>
2019-03-20 17:49:59 -0700

    a

--- static/js/app.js
+++ static/js/app.js
@@ -15,7 +15,7 @@ function renderTable() {
     // Create a new row in the tbody, set the index to be i + startingIndex
     var $row = $tbody.insertRow(i);
     for (var j = 0; j < observations.length; j++) {
-      // For every observations in the ufo object, create a new cell at set its inner text to be the current value at the current     ufo'sobservation
+      // For every observations in the ufo object, create a new cell at set its inner text to be the current value at the current ufo'sobservation
       var observation = observations[j];
       var $cell = $row.insertCell(j);
       $cell.innerText = ufo[observation];
